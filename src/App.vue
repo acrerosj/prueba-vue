2c246d22af69b844b47ba81e2cbeaef4f0e5dd12
Andrés <acrerosj@canariaseducacion.es>
2024-02-08 18:38:03 +0000

    events

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,42 @@
 <template lang="">
-  <h1>Lista de tareas</h1>
-  <CreateTask @insert-task="insertTask"/>
   <p>
-    <input type="text" placeholder="nueva tarea" @keyup.enter="insertTask" v-model="newTask">
+    Opción: 
+    <select v-model="option">
+      <option value="list">Listado</option>
+      <option value="create">Crear Tarea</option>
+    </select>
   </p>
-  <p>Nuevo id: {{newId}}</p>
-  <p>Se han encontrado {{tasks.length}} tareas</p>
-  <p>{{ textoNumeroTareas }}</p>
-  <ul>
-    <li v-for="task of tasks" 
-      :key="task.id"
-      @click="removeTask(task.id)">
-      {{task.id}} - {{task.name}}
-    </li>
-  </ul>
+  <div v-if="option=='list'" >
+    <h1>Lista de tareas</h1>
+    <p>Se han encontrado {{tasks.length}} tareas</p>
+    <p>{{ textoNumeroTareas }}</p>
+    <ul>
+      <ViewTask 
+        v-for="task of tasks"
+        :key="task.id"
+        :task="task"
+        @remove-task="removeTask"
+        />
+    </ul>
+    <ul>
+      <li v-for="task of tasks" 
+        :key="task.id"
+        @click="removeTask(task.id)">
+        {{task.id}} - {{task.name}}
+      </li>
+    </ul>
+  </div>
+  <CreateTask 
+    v-else
+    @insert-task="insertTask"/>
 </template>
 
 <script setup>
 import { computed, ref } from 'vue';
 import CreateTask from './components/CreateTask.vue';
+import ViewTask from './components/ViewTask.vue';
 
+const option = ref('list');
 const tasks = ref([
   {id: 1, name: 'Corregir exámenes del lado del servidor'},
   {id: 2, name: 'Corregir exámenes del lado del cliente'},
@@ -35,12 +52,12 @@ const textoNumeroTareas = computed(()=>{
   else return "Hay " + tasks.value.length + " tareas."
 });
 
-function insertTask(txt) {
+function insertTask(task) {
   console.log("Insertando nueva tarea...");
-  console.log(txt);
+  console.log(task);
   tasks.value.push({
     id: newId.value,
-    name: txt
+    name: task
   });
 }
 
